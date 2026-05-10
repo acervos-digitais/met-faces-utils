f29f213518ce578a17a05213509f0a0754850601
Thiago Hersan <thiago.hersan+github@gmail.com>
2026-05-10 13:26:15 -0400

    crop faces

--- utils.py
+++ utils.py
@@ -31,6 +31,23 @@ def pxs_to_pcts(boxes, img_w, img_h):
   return (np.array(xywhn), np.array(xyxyn))
 
 
+def pct_to_px(box, img_w, img_h):
+  x0,y0,x1,y1 = box
+  return [int(x0 * img_w), int(y0 * img_h), int(x1 * img_w), int(y1 * img_h)]
+
+
+def pct_to_px_sq(box, img_w, img_h):
+  x0,y0,x1,y1 = pct_to_px(box, img_w, img_h)
+  dim = 1.1 * max(x1 - x0, y1 - y0)
+  dim_2 = dim / 2
+  cx, cy = (x0 + x1) / 2, (y0 + y1) / 2
+
+  cx = min(max(cx, dim_2), img_w - dim_2)
+  cy = min(max(cy, dim_2), img_h - dim_2)
+
+  return [int(cx - dim_2), int(cy - dim_2), int(cx + dim_2), int(cy + dim_2)]
+
+
 def draw_boxes(img, boxes):
   dimg = img.convert("RGB").copy()
   draw = PImageDraw.Draw(dimg)
