21fd8cac8ee47eefea3d501778ca276b60040eff
Thiago Hersan <thiago.hersan+github@gmail.com>
2026-06-10 00:07:42 -0300

    export polygon-cropped eyes

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import json
 import numpy as np
 
 from os import listdir
-from PIL import ImageDraw as PImageDraw
+from PIL import Image as PImage, ImageDraw as PImageDraw
 
 
 def get_masks_definitions():
@@ -100,3 +100,43 @@ def draw_boxes(img, boxes):
                    width=(min(img.size) // 128))
   dimg.thumbnail((300,300))
   return dimg
+
+
+def curve_t(a,b,c,d,t):
+  t3 = t * t * t
+  t2 = t * t
+  f1 = -0.5 * t3 + t2 - 0.5 * t
+  f2 = 1.5 * t3 - 2.5 * t2 + 1
+  f3 = -1.5 * t3 + 2 * t2 + 0.5 * t
+  f4 = 0.5 * t3 -0.5 * t2
+  return a * f1 + b * f2 + c * f3 + d * f4
+
+
+def curve_segment(a,b,c,d,detail=4):
+  return np.array([
+    curve_t(a,b,c,d,t)
+    for t in np.linspace(0.0, 1.0, num=detail)
+  ])
+
+
+def curve(points,detail=4):
+  points_np = np.vstack((points[:1], points, points[-1:]))
+  curve_points = np.empty(shape=(0,2))
+  for idx in range(0, len(points_np) - 3):
+    curve_points = np.vstack((curve_points, curve_segment(*points_np[idx:idx+4], detail=detail)))
+  return curve_points
+
+
+def crop_with_polygons(img, pointses, detail=4):
+  polygons = [
+    curve(np.vstack((points, points[:1])), detail=detail)
+    for points in pointses
+  ]
+
+  nimg = img.convert("RGBA")
+  mask = PImage.new("L", nimg.size, 0)
+  draw = PImageDraw.Draw(mask)
+  for polygon in polygons:
+    draw.polygon(np.array(polygon).tolist(), fill=255)
+  nimg.putalpha(mask)
+  return nimg
